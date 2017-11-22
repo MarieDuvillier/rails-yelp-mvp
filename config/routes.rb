c22305a1dbac8352e632dcdac4a8de7e436333d0
Marie Duvillier <marie.antonia.duvillier@gmail.com>
2017-11-22 15:12:50 +0100

    2 models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants, only: [:index, :new, :create, :show] do
+      resources :reviews, only: [ :new, :create ]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 rue Oberkampf, 75011, Paris',
+    phone_number:  '0142563726',
+    category:       'chinese'
+  },
+  {
+    name:         'PizzaChic',
+    address:      '4 place Saint Opportune, 75001, Paris',
+    phone_number:  '0146550933',
+    category:       'italian'
+  },
+  {
+    name:         'Sushiba',
+    address:      '7 rue des Annelets, 75019, Paris',
+    phone_number:  '0190987677',
+    category:       'japanese'
+  },
+  {
+    name:         'Chez Jeannot',
+    address:      '13 rue de la Planche, 75007, Paris',
+    phone_number:  '0147583944',
+    category:       'french'
+  },
+  {
+    name:         'Roger la frite',
+    address:      '68 rue de la baume, 75008, Paris',
+    phone_number:  '0178456733',
+    category:       'belgian'
+  },
+
+]
+Restaurant.create!(restaurants_attributes)
